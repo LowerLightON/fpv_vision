17991c923bd046fd88de007f10dbfccd8f8f27d3
LowerLightON <lowerlighton@gmail.com>
2026-03-14 08:00:18 +0300

    Added step Resize

--- src/fpv_vision/vision/steps/resize.py
+++ src/fpv_vision/vision/steps/resize.py
@@ -1,6 +1,13 @@
 from fpv_vision.vision.steps.base import BaseStep
 from typing import TypeVar
+import cv2
 T = TypeVar('T')
 class Resize(BaseStep[T]):
+    def __init__(self, width, height) -> None:
+        self.width = width
+        self.height = height
     def apply(self, frame: T) -> T:
+        if frame is None:
+            raise ValueError('frame is None')
+        frame = cv2.resize(frame, (self.width, self.height))
         return frame
