e30ffa26c0c5b59feb7a078b120b2276cd7468ba
LowerLightON <lowerlighton@gmail.com>
2026-05-24 20:05:12 +0300

    Add step timing aggregation to metrics collector

--- src/fpv_vision/core/metricscollector.py
+++ src/fpv_vision/core/metricscollector.py
@@ -3,6 +3,7 @@ class MetricsCollector:
     def __init__(self):
         self.step_timings: dict[str, deque[float]] = {}
         self.frame_latencies: deque[float] = deque(maxlen=120)
+
     def record_step (self, name_step: str, duration_ms: float)->None:
         if name_step not in self.step_timings:
             self.step_timings[name_step] = deque(maxlen=120)
@@ -12,4 +13,28 @@ class MetricsCollector:
         self.frame_latencies.append(total_latency_ms)
     
     def get_snapshot(self)->dict:
-        pass
+        if not self.frame_latencies:
+              frame_last = 0.0
+              frame_avg = 0.0
+        else:
+            frame_last = self.frame_latencies[-1]
+            frame_avg = sum(self.frame_latencies) / len(self.frame_latencies)
+
+        step_snapshot = {}
+
+        for step_name, timings in self.step_timings.items():
+            step_last = timings[-1]
+            step_avg = sum(timings) / len(timings)
+
+            step_snapshot[step_name] = {
+                "last" : step_last,
+                "avg" : step_avg
+            }
+
+        return {
+            "frame_latency": {
+                "last" : frame_last,
+                "avg" : frame_avg
+            },
+            "steps" : step_snapshot
+        }
